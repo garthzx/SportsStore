4bae717dbc2f1aa473f0e841aa73080e809ebfda
Garth Dustin P. Ayang-ang <gdayang-ang@pdc.ebasph.com>
2024-10-22 10:24:08 +0800

    revert again

---
+++ backend/SportsStore/SportsStore/Api/CartController.cs
@@ -0,0 +1,28 @@
+﻿using BusinessLogicLayer;
+using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SportsStore.Api
+{
+    public class CartController : ApiController
+    {
+        private readonly SportsStoreBusinessLogicLayer bll;
+
+        public CartController()
+        {
+            bll = new SportsStoreBusinessLogicLayer("connTraining");
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetAllCartItemsOfUser(Guid userId)
+        {
+            var cartIdOfUser = StaticDAL.Carts.FirstOrDefault(u => u.UserId.Equals(userId));
+            return Ok(StaticDAL.CartItems.Where(item => item.CartId.Equals(cartIdOfUser)).ToList());
+        }
+    }
+}
